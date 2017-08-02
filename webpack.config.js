f74c2a299de6f5783f8fe8b47bbac670270503cd
Nick Bumbarger <nbumbarger@gmail.com>
2017-08-02 18:08:58 -0400

    Add build/serve/live-reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
-module.exports = {
-  entry: {
-    helloWorld: './js/helloworld'
-  },
+var process = require('process');
+
+function getEntrySources(sources) {
+  if (process.env.NODE_ENV !== 'production') {
+    sources.push('webpack-dev-server/client?http://localhost:8080');
+  }
+
+  return sources;
+}
+
+module.exports = function() {
+  return {
+    entry: {
+      helloWorld: getEntrySources([
+        './js/helloworld'
+      ])
+    },
     output: {
-        filename: './public/[name].js'
+      filename: './public/[name].js'
     }
+  };
 };
