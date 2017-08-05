9ca4622dbc8283caa78c0cff87ac53710332d49d
Nick Bumbarger <nbumbarger@gmail.com>
2017-08-05 13:09:36 -0400

    Upgrade hot loader to simplify configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
-var process = require('process');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-function getEntrySources(sources) {
-  if (process.env.NODE_ENV !== 'production') {
-    sources.push('webpack-dev-server/client?http://localhost:8080');
-    sources.push('webpack/hot/only-dev-server');
-  }
-
-  return sources;
-}
+const webpack = require('webpack');
+const process = require('process');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
-
-module.exports = {
-  entry: {
-    bundle: getEntrySources([
-      './js/app'
-    ])
-  },
-  output: {
-    publicPath: 'http://localhost:8080/',
-    filename: './public/[name].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: [
-          'react-hot-loader',
-          'jsx-loader',
-          'babel-loader?presets[]=latest&presets[]=react'
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'sass-loader'
-          ]
-        })
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin('public/bundle.css', {
-      allChunks: true
-    })
-  ]
-};
+  module.exports = {
+    entry: {
+     app: './js/app'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './public',
+      hot: true
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loaders: [
+            'react-hot-loader',
+            'jsx-loader',
+            'babel-loader?presets[]=latest&presets[]=react'
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              'sass-loader'
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('assets/main.css', {
+        allChunks: true
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Hot Module Replacement'
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+      filename: 'assets/bundle.js',
+      path: path.resolve(__dirname, 'public')
+    }
+  };
